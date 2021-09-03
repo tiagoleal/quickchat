211799c186ca0403e5e1398e8dba096907e61d4b
Moacir Petry <m.petry9@gmail.com>
2021-09-03 15:18:05 -0300

    test: add models tests

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'Associations' do
+    it 'belongs_to? user' do
+      is_expected.to belong_to(:user)
+    end
+    it 'has_many? talks' do
+      is_expected.to have_many(:talks)
+    end
+    it 'has_many? channels' do
+      is_expected.to have_many(:channels)
+    end
+    it 'has_many? team_users' do
+      is_expected.to have_many(:team_users)
+    end
+    it 'has_many? users' do
+      is_expected.to have_many(:users)
+    end
+  end
+
+  context 'Validates fields' do
+    it 'slug?' do
+      is_expected.to validate_presence_of(:slug)
+    end
+    it 'user?' do
+      is_expected.to validate_presence_of(:user)
+    end
+    # FIXME Uncomment and test
+    # it 'validate_uniqueness_of? slug' do
+    #   is_expected.to validate_uniqueness_of(:slug)
+    # end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'Associations' do
+    it 'has_many? teams' do
+      is_expected.to have_many(:teams)
+    end
+    it 'has_many? messages' do
+      is_expected.to have_many(:messages)
+    end
+    # TODO It's not working, maybe because Talks has user_one and user_to, it needed to be validate
+    # it 'has_many? talks' do
+    #   is_expected.to have_many(:talks)
+    # end
+    it 'has_many? team_users' do
+      is_expected.to have_many(:team_users)
+    end
+    it 'has_many? member_teams' do
+      is_expected.to have_many(:member_teams)
+    end
+  end
 end
